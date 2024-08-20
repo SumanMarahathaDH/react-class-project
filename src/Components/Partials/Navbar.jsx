c488e8152bb2023c0de0a6f032b43f9adc3e666e
SumanMarahathaDH <smarahatta@deerhold.com>
2024-08-20 18:42:55 +0545

    added logo image

--- src/Components/Partials/Navbar.jsx
+++ src/Components/Partials/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { logout } from '../../Pages/Authentication/services'
 import Button from '../Common/Button'
 
@@ -6,7 +7,7 @@ const Navbar = () => {
   return (
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" style={{fontSize: '1.1rem'}} href="#">Movie Application</a>
+    <Link className="navbar-brand" to="/"><img src="images/logo.svg" className='logo' alt="LOGO" /></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
